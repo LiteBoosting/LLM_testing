2b43a8290ed5be2ebc4cd292043ab8016fdd9211
刘森茂 <liusenmao@U-3WH49Q6D-2153.local>
2023-12-15 17:15:01 +0800

    modify test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 # this is a testing python file, what if we directly use this as IDE?
 a = 1
 a = a + 1
+
+# this time we add a new simple function to start some developing work
+def take_skewed_sum(x1, x2, skewness=2):
+    return x1 + x2 * skewness
